b24e2d204c95a16ae52bf78fb8ec4a946fc99844
MagicKefiR <3939723@gmail.com>
2023-03-22 16:16:21 +0300

    ver 1.0

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,9 +1,12 @@
 import logging
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 
+import oge_parser
 
 logging.basicConfig(
+    filename='log_oge_results.txt',
+    filemode='w',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 
@@ -11,32 +14,35 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Введи фамилию одноклассника"
-    )
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"Привет, {update.message.from_user.first_name}\! \nЧтобы получить результаты, "
+                                        "отправьте данные в следующем формате: \n"
+                                        "_Иванов 4019 112233_", parse_mode="MarkdownV2")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="||пошел нахуй||",
-        parse_mode="MarkdownV2"
-    )
-    message = f'{update.message.text} — {update.message.from_user.full_name}'
-    print(message)
-    f = open("log.txt", "a")
-    f.write(f'{message}\n')
-    f.close()
+async def getResults(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    surname, series, number = update.message.text.split(" ")
+    print(f'{surname} {series} {number} {update.message.from_user.full_name}')
+    try:
+        results = oge_parser.OGE(surname, series, number).value
+    except ValueError:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="В базе совпадений не найдено.\nОтправь снова, прочитав еще раз :)\nБот ссылается на ege.spb.ru!\n")
+    else:
+        s = ""
+        for one_result in results:
+            s += f'{one_result["subject"]} -> {one_result["mark"]}\n'
+        # print(results)
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=s)
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5395167161:AAH8xa8wSZRyP2TfMS_62X-EOqPa-sUvDOI').build()
 
     start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-
+    getResults_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), getResults)
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
+    application.add_handler(getResults_handler)
 
     application.run_polling()
--- oge_parser.py
+++ oge_parser.py
@@ -3,49 +3,54 @@ from bs4 import BeautifulSoup
 import urllib.parse
 
 
-def backmark(r):
-    soup = BeautifulSoup(r.text, 'lxml')
-    mark = soup.select(".exam-result")
+class OGE:
+    def backmark(self, r):
+        soup = BeautifulSoup(r.text, 'lxml')
+        mark = soup.select(".exam-result")
 
-    for i in range(len(mark)):
-        mark[i] = mark[i].get_text()
-        mark[i] = mark[i].replace("\n", "")
-        mark[i] = mark[i].replace("\t", "")
+        for i in range(len(mark)):
+            mark[i] = mark[i].get_text()
+            mark[i] = mark[i].replace("\n", "")
+            mark[i] = mark[i].replace("\t", "")
 
-    return mark
+        return mark
 
+    def backobject(self, r):
+        soup = BeautifulSoup(r.text, 'lxml')
+        object = soup.select(".exam-subject-info")
 
-def backobject(r):
-    soup = BeautifulSoup(r.text, 'lxml')
-    object = soup.select(".exam-subject-info")
+        for i in range(len(object)):
+            object[i].span.decompose()
+            object[i] = object[i].get_text()
+            object[i] = object[i].replace("\xa0", "")
 
-    for i in range(len(object)):
-        object[i].span.decompose()
-        object[i] = object[i].get_text()
-        object[i] = object[i].replace("\xa0", "")
+        return object
 
-    return object
+    def backsite(self, family, series, number):
+        f = {'pLastName': family.encode('windows-1251'), 'Series': str(series), 'Number': str(number),
+             'Login': 'Показать результаты'}
+        data = urllib.parse.urlencode(f)
+        url = 'https://www.ege.spb.ru/result/index.php?mode=gia2022&wave=1'
+        headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Origin': 'https://www.ege.spb.ru'}
+        return requests.post(url, data=data, headers=headers)
 
+    def composition(self, subject, mark):
+        results = []
 
-def backsite(family, series, number):
-    f = {'pLastName': family.encode('windows-1251'), 'Series': str(series), 'Number': str(number),
-         'Login': 'Показать результаты'}
-    data = urllib.parse.urlencode(f)
-    url = 'https://www.ege.spb.ru/result/index.php?mode=gia2022&wave=1'
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Origin': 'https://www.ege.spb.ru'}
-    return requests.post(url, data=data, headers=headers)
+        for i in range(len(subject)):
+            results.append({"subject": subject[i], "mark": mark[i]})
+        return results
 
+    def __init__(self, surname, serial, number):
 
-def main():
-    r = backsite(input("Введите фамилию: "), input("Введите серию паспорта: "), input("Введите номер паспорта: "))
+        r = self.backsite(surname, int(serial), int(number))
 
-    object = backobject(r)
-    mark = backmark(r)
+        object = self.backobject(r)
+        mark = self.backmark(r)
 
+        # print(object)
+        # print(mark)
 
-    print(object)
-    print(mark)
-
-
-if __name__ == '__main__':
-    main()
+        self.value = self.composition(object, mark)
+        if not self.value:
+            raise ValueError("Неверные данные")
